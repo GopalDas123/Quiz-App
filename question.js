a01a2f3ac25032538472896f864747c2ab039818
GopalDas123 <Gopaldax@gmail.com>
2020-07-20 13:17:42 +0500

    Making mainQuiz.js

---
+++ question.js
@@ -0,0 +1,29 @@
+var questions=[{
+    "question":"What is the color of Pakistan's Flag? ",
+    "option1":"green & red",
+    "option2":"green & white",
+    "option3":"blue & red",
+    "option4":"white & red",
+    "answer":"2"
+},{
+    "question":"What is the population of Pakistan? ",
+    "option1":"300 Million",
+    "option2":"430 Million",
+    "option3":"120 Million",
+    "option4":"None of above",
+    "answer":"4"
+},{
+    "question":"What is the population of world? ",
+    "option1":"7 Billion",
+    "option2":"5.6 Billion",
+    "option3":"12 Billion",
+    "option4":"None of above",
+    "answer":"1"
+},{
+    "question":"Italy is the country of? ",
+    "option1":"Asia",
+    "option2":"Europe",
+    "option3":"Africa",
+    "option4":"None of above",
+    "answer":"2"
+}]
